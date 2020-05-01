4d08c429d9ca3280ef293017f7b532862c809d91
Anurag Kashyap <anurag.95.kashyap@gmail.com>
2020-05-02 03:07:18 +0530

    adding mongoDB, routes and users model

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// connect Database
+connectDB();
+
+// init middleware
+app.use(express.json({
+    extended: false
+}));
 
 app.get('/', (req, res) => {
     res.send('API running');
 })
 
+app.use('/users', require('./routes/api/users'));
+app.use('/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
-})
+});
