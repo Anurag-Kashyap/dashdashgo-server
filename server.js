8f9232ab5b9a87d8281b175aa5395f49e9cad12c
Anurag Kashyap <anurag.95.kashyap@gmail.com>
2020-05-08 09:54:40 +0530

    add user onboarding tracker API

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use("/profile", require("./routes/api/profile"));
 app.use("/category", require("./routes/api/category"));
 app.use("/apps", require("./routes/api/apps"));
 app.use("/frequent-apps", require("./routes/api/frequentApps"));
+app.use("/track-registration", require("./routes/api/registrationTracker"));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('./build'));
